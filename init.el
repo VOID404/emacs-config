8f42f94bd2e89f85ef7a147c72ff862a7e43e450
Wojciech Nawa <wojciech.nawa@gmail.com>
2018-10-10 15:51:22 +0200

    Migrate to Cask

--- init.el
+++ init.el
@@ -1,53 +1,3 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(require 'subr-x)
-(straight-use-package 'git)
-
-(defun org-git-version ()
-  "The Git version of 'org-mode'.
-Inserted by installing 'org-mode' or when a release is made."
-  (require 'git)
-  (let ((git-repo (expand-file-name
-		   "straight/repos/org/" user-emacs-directory)))
-    (string-trim
-     (git-run "describe"
-	      "--match=release\*"
-	      "--abbrev=6"
-	      "HEAD"))))
-
-(defun org-release ()
-  "The release version of 'org-mode'.
-Inserted by installing 'org-mode' or when a release is made."
-  (require 'git)
-  (let ((git-repo (expand-file-name
-                   "straight/repos/org/" user-emacs-directory)))
-    (string-trim
-     (string-remove-prefix
-      "release_"
-      (git-run "describe"
-               "--match=release\*"
-               "--abbrev=0"
-               "HEAD")))))
-
-(provide 'org-version)
-
-(straight-use-package 'org) ; or org-plus-contrib if desired
-
-(org-babel-load-file
- (expand-file-name "settings.org"
-                   user-emacs-directory))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -61,5 +11,12 @@ Inserted by installing 'org-mode' or when a release is made."
  ;; If there is more than one, they won't work right.
  )
 
-(provide 'init)
-;;; init.el ends here
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+
+(evil-mode 1)
+
+(org-babel-load-file
+ (expand-file-name "settings.org"
+		   user-emacs-directory))
