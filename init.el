93585a84e27f2e97b79f98f6bc6cf721484aaaeb
Wojciech Nawa <wojciech.nawa@gmail.com>
2019-07-09 02:20:05 +0200

    Revert to straight.el Loosing all changes made since Cask migration

--- init.el
+++ init.el
@@ -1,15 +1,59 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(require 'subr-x)
+(straight-use-package 'git)
+
+(defun org-git-version ()
+  "The Git version of 'org-mode'.
+Inserted by installing 'org-mode' or when a release is made."
+  (require 'git)
+  (let ((git-repo (expand-file-name
+		   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (git-run "describe"
+	      "--match=release\*"
+	      "--abbrev=6"
+	      "HEAD"))))
+
+(defun org-release ()
+  "The release version of 'org-mode'.
+Inserted by installing 'org-mode' or when a release is made."
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (string-remove-prefix
+      "release_"
+      (git-run "describe"
+               "--match=release\*"
+               "--abbrev=0"
+               "HEAD")))))
+
+(provide 'org-version)
+
+(straight-use-package 'org) ; or org-plus-contrib if desired
+
+(org-babel-load-file
+ (expand-file-name "settings.org"
+                   user-emacs-directory))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("dd4628d6c2d1f84ad7908c859797b24cc6239dfe7d71b3363ccdd2b88963f336" "0c3b1358ea01895e56d1c0193f72559449462e5952bded28c81a8e09b53f103f" default)))
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (epresent yaml-mode winum which-key web-mode use-package sudo-edit spaceline smex smartparens rust-snippets rjsx-mode racer prodigy popwin pallet ox-reveal ob-restclient nyan-mode neotree multiple-cursors minimap linum-relative iflipb idle-highlight-mode htmlize general flycheck-rust flycheck-cask expand-region exec-path-from-shell evil-magit ensime drag-stuff dracula-theme dockerfile-mode counsel-projectile company-restclient base16-theme autopair ace-window ace-jump-buffer))))
+ '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,12 +61,5 @@
  ;; If there is more than one, they won't work right.
  )
 
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-
-
-(evil-mode 1)
-
-(org-babel-load-file
- (expand-file-name "settings.org"
-		   user-emacs-directory))
+(provide 'init)
+;;; init.el ends here
