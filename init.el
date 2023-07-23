7f97db1c65b74ae09f27fd0a277d74aaa70b32bd
void404 <wojciech.nawa@gmail.com>
2023-07-23 19:56:38 +0200

    Move to use-package

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(setq package-enable-at-startup nil)
+
 (set-foreground-color "white")
 (set-background-color "black")
 
@@ -22,42 +24,8 @@
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
 ;; ELPACA
-(defvar elpaca-installer-version 0.3)
-(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil
-                              :files (:defaults (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
-    (make-directory repo t)
-    (condition-case-unless-debug err
-        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (call-process "git" nil buffer t "clone"
-                                       (plist-get order :repo) repo)))
-                 ((zerop (call-process "git" nil buffer t "checkout"
-                                       (or (plist-get order :ref) "--"))))
-                 (emacs (concat invocation-directory invocation-name))
-                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                 ((require 'elpaca))
-                 ((elpaca-generate-autoloads "elpaca" repo)))
-            (kill-buffer buffer)
-          (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
-(add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca `(,@elpaca-order))
-;; ELPACA
+(defvar elpaca-installer-version 0.5)
+(load (expand-file-name "elpaca.el" user-emacs-directory))
 
 (defun meow-setup ()
   (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
@@ -147,10 +115,11 @@
    '("'" . repeat)
    '("<escape>" . ignore)))
 
-(elpaca meow
-  (require 'meow)
-  (meow-setup)
-  (meow-global-mode))
+(elpaca elpaca-use-package
+  ;; Enable :elpaca use-package keyword.
+  (elpaca-use-package-mode)
+  ;; Assume :elpaca t unless otherwise specified.
+  (setq elpaca-use-package-by-default t))
 
 (elpaca org
   (require 'org))
@@ -158,6 +127,12 @@
 (elpaca general
   (require 'general))
 
+(elpaca meow
+  (require 'meow)
+  (meow-setup)
+  (meow-global-mode))
+
+;; Block until current queue processed.
 (elpaca-wait)
 
 (org-babel-load-file
